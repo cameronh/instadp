72c6bceb9b0478c9e6a1ab1656bc2a79f4a5879e
Cameron Hudson <cam@chudson.io>
2020-09-15 11:40:55 -0700

    initial commit

---
+++ __tests__/index.spec.js
@@ -0,0 +1,22 @@
+const InstaDP = require('../lib');
+const fs = require('fs');
+const path = require('path');
+const storiesHtml = fs.readFileSync(path.join(__dirname, '_stories.html'), 'utf8')
+const instadp = new InstaDP();
+
+describe('InstaDP', () => {
+  describe('_fetchStoryMediaFromHTML', () => {
+    test('should throw a TypeError on invalid html', () => {
+      expect(() => { instadp._fetchStoryMediaFromHTML(null); }).toThrow(TypeError);
+      expect(() => { instadp._fetchStoryMediaFromHTML(null); }).toThrow(/Invalid HTML/);
+    });
+
+    test('should return an array with 2 story items containing an image and video', () => {
+      const stories = instadp._fetchStoryMediaFromHTML(storiesHtml);
+
+      expect(stories).toHaveLength(2);
+      expect(stories[0]).toStrictEqual('image.jpg');
+      expect(stories[1]).toStrictEqual('video.mp4');
+    });
+  });
+});
