0dc027a604a97dbad613d5fdec8ffb89a1d0945b
Cameron Hudson <cam@chudson.io>
2020-09-15 13:59:51 -0700

    added getStories test

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -1,22 +1,31 @@
 const InstaDP = require('../lib');
 const fs = require('fs');
 const path = require('path');
-const storiesHtml = fs.readFileSync(path.join(__dirname, '_stories.html'), 'utf8')
-const instadp = new InstaDP();
+const storiesHtml = fs.readFileSync(path.join(__dirname, '_stories.html'), 'utf8');
+
+const mockFetcher = async (url) => {
+  return { text: async () => storiesHtml }
+}
+
+const mockFetcherNull = async (url) => {
+  return { text: async () => null }
+}
+
+const instadp = new InstaDP(mockFetcher);
+const instadpFail = new InstaDP(mockFetcherNull);
 
 describe('InstaDP', () => {
-  describe('_fetchStoryMediaFromHTML', () => {
-    test('should throw a TypeError on invalid html input', () => {
-      expect(() => { instadp._fetchStoryMediaFromHTML(null); }).toThrow(TypeError);
-      expect(() => { instadp._fetchStoryMediaFromHTML(null); }).toThrow(/Invalid HTML/);
-    });
-
-    test('should return an array with 2 story items containing an image and video', () => {
-      const stories = instadp._fetchStoryMediaFromHTML(storiesHtml);
+  describe('getStories', () => {
+    test('should return an array with 2 story items containing an image and video', async () => {
+      const stories = await instadp.getStories('foo');
 
       expect(stories).toHaveLength(2);
       expect(stories[0]).toStrictEqual('image.jpg');
       expect(stories[1]).toStrictEqual('video.mp4');
+    });
+
+    test('should throw an error for invalid html input', async () => {
+      await expect(instadpFail.getStories('foo')).rejects.toThrow(TypeError);
     });
   });
 });
